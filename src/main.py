c18b816f6a9f99df9efd676734134307b38834ad
datalexum <alexander@schroeder-de.com>
2022-07-14 17:41:44 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import secrets_config
+from app.libs.ota_updater import OTAUpdater
+from app.start import main
+
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/datalexum/Smart-ATO-Micropython', main_dir='src/app')
+    o.install_update_if_available_after_boot(secrets_config.WIFI_SSID, secrets_config.WIFI_PASSWORD)
+
+
+def start():
+    main()
+
+
+def boot_showerloop():
+    download_and_install_update_if_available()
+
+
+s = None
+boot_showerloop()
